389768cf227ee532f04972a740e3922f7c99ca01
zhanxw <zhanxw@gmail.com>
2015-10-07 23:58:05 -0400

    add makeChunk()

--- src/XiaoweiLib.py
+++ src/XiaoweiLib.py
@@ -699,3 +699,12 @@ def flatten(x):
         return [a for i in x for a in flatten(i)]
     else:
         return [x]
+
+# get chunks of lists
+# http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks-in-python
+def makeChunk(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+       yield l[i:i+n]
+
+
